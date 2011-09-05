102966eba0679bfb854823174d279f91a6d288a7
Stefan Forsberg <cykelkatalogen@popkram.com>
2011-09-05 16:10:48 +0200

    Added specs for ContactUs

---
+++ src/EPiTest.UI.AlloyTechSample/ContactUsSpecs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EPiTest.WebDriverExtension;
+using Machine.Specifications;
+
+namespace EPiTest.UI.AlloyTechSample
+{
+    public class when_entering_all_requiered_fields_and_submitting : UiTestBase
+    {
+        Establish context = () =>
+            {
+                Visit("/Contact-Us/");
+            };
+
+        Because of = () =>
+            {
+                Driver.Within("[id$='FormPanel']", e =>
+                    {
+                        e.FillIn("Your name:", "Stefan");
+                        e.FillIn("Your e-mail:", "some@thing.com");
+                        e.FillIn("Your message:", "Hello. This is my message");
+                        e.ClickButton("Send");
+                    });
+            };
+
+        It should_display_a_thank_you_message = () =>
+            {
+                Driver.ShouldHaveCss(".thankyoumessage");
+            };
+    }
+
+    public class when_a_requiered_field_is_not_given_a_value : UiTestBase
+    {
+        Establish context = () =>
+        {
+            Visit("/Contact-Us/");
+        };
+
+        Because of = () =>
+        {
+            Driver.Within("[id$='FormPanel']", e =>
+            {
+                e.FillIn("Your name:", "Stefan");
+                e.FillIn("Your e-mail:", "some@thing.com");
+                e.ClickButton("Send");
+            });
+        };
+
+        It should_indicate_that_the_field_must_have_a_value = () =>
+        {
+            Driver.ShouldHaveCss("[id$='FormPanel'] tr:nth-child(3) .xformvalidator");
+        };
+    }
+}
