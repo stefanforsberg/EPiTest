820fb7adff97ee9e83e9f5b7e2c1e69daf5b1a4f
Stefan Forsberg <cykelkatalogen@popkram.com>
2011-09-12 13:08:56 +0200

    Added install script

---
+++ src/EPiTest.UI/Tools/Install.ps1
@@ -0,0 +1,3 @@
+ param($installPath, $toolsPath, $package, $project)
+ 
+ write-host "Welcome to EPiTest.UI Remember to update the variables in app.config to reflect the site you're testing."  -foreground "magenta"
