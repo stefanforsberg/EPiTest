8ec58d6d9e52d14b87f41585e562d8e7596503cb
Stefan Forsberg <cykelkatalogen@popkram.com>
2011-09-15 21:16:03 +0200

    Copies value from EpiServer site if one is found

--- src/EPiTest.UI/Tools/Install.ps1
+++ src/EPiTest.UI/Tools/Install.ps1
@@ -1,3 +1,25 @@
- param($installPath, $toolsPath, $package, $project)
+param($installPath, $toolsPath, $package, $project)
  
- write-host "Welcome to EPiTest.UI Remember to update the variables in app.config to reflect the site you're testing."  -foreground "magenta"
+$epiProjectFolder = ""
+$project.Globals.DTE.Solution.Projects | ForEach-Object {
+    $dir = ([System.IO.FileInfo]$_.FullName).DirectoryName
+    if(test-path "$dir\EPiServer.config") { $epiProjectFolder = $dir }
+}
+ 
+ if($epiProjectFolder -ne "") {
+    Write-Host "Found an EPiServer project in the solution. Trying to set site url and UI-url." -ForegroundColor magenta
+    
+    $epiconfig = [xml](get-content "$epiProjectFolder\episerver.config")    
+    
+    $uiConfigPath = ([System.IO.FileInfo]$project.FullName).DirectoryName
+    
+    $uiTestConfig = [xml](get-content "$uiConfigPath\app.config")    
+    
+    $uiTestConfig.configuration.appSettings.add[0].value = $webConfigFile.episerver.sites.site.siteSettings.siteUrl
+    $uiTestConfig.configuration.appSettings.add[1].value = $webConfigFile.episerver.sites.site.siteSettings.uiUrl
+    
+    $uiTestConfig.Save("$uiConfigPath\app.config")
+}
+else {
+    write-host "Welcome to EPiTest.UI Remember to update the variables in app.config to reflect the site you're testing."  -foreground "magenta"
+}
