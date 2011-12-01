72fde06286fc44a14e0136bb574f6cc8916b2ebe
joelabrahamsson <mail@joelabrahamsson.com>
2011-12-01 17:32:49 +0100

    Added support for supplying a custom method as host name resolver in EPiServerSetup.StartUp and support for changing start page between tests. The latter is achieved by changing the site ID and clearing the backing field for PageReference.StartPage.

--- src/EPiTest.DB/EPiServerSetup.cs
+++ src/EPiTest.DB/EPiServerSetup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Reflection;
 using EPiServer;
 using EPiServer.BaseLibrary;
 using EPiServer.ChangeLog;
@@ -16,11 +17,24 @@ namespace EPiTest.DB
 {
     public class EPiServerSetup
     {
-        public static void StartUp()
+        public static void StartUp(Func<string> hostNameResolver = null)
         {
             if (string.IsNullOrEmpty(AppDomain.CurrentDomain.RelativeSearchPath))
             {
                 AppDomain.CurrentDomain.AppendPrivatePath(AppDomain.CurrentDomain.BaseDirectory);
+            }
+
+            if (hostNameResolver == null)
+            {
+                hostNameResolver = () => "*";
+            }
+            SiteMappingConfiguration.CurrentHostNameResolver = hostNameResolver;
+
+            if (SiteMappingConfiguration.Instance != null)
+            {
+                SiteMappingConfiguration.Instance.SiteId =
+                    SiteMappingConfiguration.Instance.SiteIdForHost(hostNameResolver());
+                typeof(PageReference).GetField("_start", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, null);
             }
 
             GenericHostingEnvironment.Instance = new EPiServerHostingEnvironment();
@@ -28,7 +42,6 @@ namespace EPiTest.DB
             Global.BaseDirectory = ".";
             Global.InstanceName = "EPiServer Unit Test";
 
-            SiteMappingConfiguration.CurrentHostNameResolver = () => "*";
             InitializationModule.FrameworkInitialization(HostType.WebApplication);
 
             Settings.InitializeAllSettings(ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None));
